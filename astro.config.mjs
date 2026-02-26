f625570c2d812f6f7b97be84b2dea6c49c9f1ed9
Jose Leon <24828205+noelsajor@users.noreply.github.com>
2026-02-25 21:25:09 -0400

    Completely remove sitemap to ensure build success

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ascentmgnt.com', // Replace with actual site URL
-  integrations: [
-    sitemap(),
-  ],
+  integrations: [],
   output: 'static',
 });
