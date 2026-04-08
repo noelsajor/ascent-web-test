427fd103512fc0ed0d001113dd9c0cee7f98ddf4
Jose Leon <24828205+noelsajor@users.noreply.github.com>
2026-04-08 12:37:31 -0400

    Add @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://ascentmgnt.com', // Replace with actual site URL
-  integrations: [],
+  site: 'https://ascentmgnt.com',
+  integrations: [sitemap()],
   output: 'static',
 });
